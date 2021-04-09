f4777b4d2485015f33fe92abe822dfb206ce4109
Vinay Sangwan <73075190+vinay4194@users.noreply.github.com>
2021-04-09 21:22:38 +0530

    Add files via upload

---
+++ 4-NavBar/index.js
@@ -0,0 +1,8 @@
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener('click',function(){
+   links.classList.toggle("show-links");
+})
+
+
